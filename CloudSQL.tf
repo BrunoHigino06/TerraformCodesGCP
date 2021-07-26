2f53da42841534171fdf498f7f29dd3a4b1d5d27
BrunoHigino06 <72587932+BrunoHigino06@users.noreply.github.com>
2021-07-25 23:24:37 -0300

    Add files via upload

--- CloudSQL.tf
+++ CloudSQL.tf
@@ -1,20 +1,20 @@
 provider "google" {
- credentials = file(var.credentials)
+ credentials = file(var.Provider)
  project     = "prod-320818"
  region      = "us-central1"
 }
 resource "google_sql_database" "database" {
-  name     = "prd_SQL"
+  name     = "prd-database-sql"
   instance = google_sql_database_instance.instance.name
 }
 
 resource "google_sql_database_instance" "instance" {
-  name   = "prd_SQL"
+  name   = "prd-sql"
   database_version = "SQLSERVER_2017_STANDARD"
   region = "us-central1"
   root_password = "040188"
   settings {
-    tier = "db-custom-2-4"
+    tier = "db-custom-2-4096"
   }
 
   deletion_protection  = "false"
